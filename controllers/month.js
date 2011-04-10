0b0426a7d43188c08c233e6fbdf56d35796a96c4
Tj Holowaychuk <tj@vision-media.ca>
2011-04-10 14:21:57 -0700

    restructure for month support

--- controllers/month.js
+++ controllers/month.js
@@ -2,6 +2,7 @@
 exports.show = function(req, res){
   var month = parseInt(req.params.month, 10)
     , items = db.months[month].items;
-  res.expose({ month: month });
-  res.render('month', { items: items });
+
+  res.expose({ month: month })
+     .render('month', { month: month, items: items });
 };
